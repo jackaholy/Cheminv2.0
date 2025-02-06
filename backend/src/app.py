4c63bd341d54ab39b1c513d59e3ef5ea1c4cd36e
Justin Forseth <justin@jforseth.com>
2025-02-06 16:21:37 -0700

    I think this works

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,10 +2,10 @@ from flask import Flask
 from sqlalchemy import URL, Table
 from sqlalchemy.orm import DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
+from flask_oidc import OpenIDConnect
 import os
+print("Starting up")
 
-load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = URL.create(
     drivername="mysql+mysqlconnector",
@@ -14,7 +14,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = URL.create(
     host="mysql",
     database=os.getenv("MYSQL_DATABASE")
 )
+
+app.config['SECRET_KEY'] = os.getenv("CHEMINV_SECRET_KEY")
+
+# Minimal OIDC configuration using environment variables.
+# When the issuer supports discovery, Flask‑OIDC will automatically retrieve the metadata
+# from: <OIDC_ISSUER> + "/.well-known/openid-configuration"
+app.config['OIDC_CLIENT_SECRETS'] = {
+    "web": {
+        "client_id": os.environ.get("CHEMINV_OIDC_CLIENT_ID"),
+        "client_secret": os.environ.get("CHEMINV_OIDC_CLIENT_SECRET"),
+        "issuer": os.environ.get("CHEMINV_OIDC_ISSUER"),  # e.g. "https://your-idp.example.com"
+        "redirect_uris": [
+            os.environ.get("CHEMINV_OIDC_REDIRECT_URI")
+        ]
+    }
+}
+
+# Additional settings
+app.config['OIDC_SCOPES'] = "openid email profile"
+app.config.setdefault("OIDC_COOKIE_SECURE", False)
+
+
 db = SQLAlchemy(app)
+oidc = OpenIDConnect(app)
 
 # There's probably a better way to do this: https://stackoverflow.com/questions/39955521/sqlalchemy-existing-database-query
 class Base(DeclarativeBase):
@@ -29,6 +52,7 @@ def hello_world():
     return 'Hello World!'
 
 @app.route('/users')
+@oidc.require_login
 def get_users():
     return "<br/>".join([user.User_Name for user in db.session.query(User).all()])
 
