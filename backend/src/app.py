9af11f06fb1a0c176d41f44380b34df67c62e21d
Justin Forseth <justin@jforseth.com>
2025-02-09 14:26:19 -0700

    Start working on defining SQLAlchemy models for the tables in Steve Harper's database

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,8 @@
 from flask import g, Flask
-from sqlalchemy import URL, Table
+from sqlalchemy import URL, Table,  Column, Integer, String, Float, Date, ForeignKey, Boolean
 from sqlalchemy.exc import DatabaseError
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 from flask_oidc import OpenIDConnect
 import os
@@ -43,30 +44,43 @@ app.config.setdefault("OIDC_COOKIE_SECURE", False)
 
 
 db = SQLAlchemy(app)
-ready = False
-while not ready:
-    try:
-        with app.app_context():
-            db.engine.connect()
-        ready = True
-    except DatabaseError as e:
-        try:
-            print("Database not ready, retrying...")
-            print(e)
-            time.sleep(1)
-            pass
-        except KeyboardInterrupt:
-            exit()
-    except Exception:
-        raise
 oidc = OpenIDConnect(app)
+Base = declarative_base()
+
+class Chemical(Base):
+    __tablename__ = 'Chemical'
+    
+    Chemical_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Chemical_Name = Column(String(90), nullable=False)
+    Chemical_Formula = Column(String(70), nullable=True)
+    Storage_Class_ID = Column(Integer, ForeignKey('Storage_Class.Storage_Class_ID'), nullable=False)
+    Alphabetical_Name = Column(String(65), nullable=False)
+    Order_More = Column(Boolean, nullable=True)
+    Order_Description = Column(String(70), nullable=True)
+    Who_Requested = Column(String(20), nullable=True)
+    When_Requested = Column(Date, nullable=True)
+    Date_Ordered = Column(Date, nullable=True)
+    Who_Ordered = Column(String(20), nullable=True)
+    Minimum_On_Hand = Column(Float, nullable=True)
+    Minimum_Unit_ID = Column(Integer, ForeignKey('Unit.Unit_ID'), nullable=True)
+    
+    storage_class = relationship("StorageClass", back_populates="chemicals")
+    minimum_unit = relationship("Unit", back_populates="chemicals")
+
+class StorageClass(Base):
+    __tablename__ = 'Storage_Class'
+    
+    Storage_Class_ID = Column(Integer, primary_key=True)
+    chemicals = relationship("Chemical", back_populates="storage_class")
+
+class Unit(Base):
+    __tablename__ = 'Unit'
+    
+    Unit_ID = Column(Integer, primary_key=True)
+    chemicals = relationship("Chemical", back_populates="minimum_unit")
 
 # There's probably a better way to do this: https://stackoverflow.com/questions/39955521/sqlalchemy-existing-database-query
-class Base(DeclarativeBase):
-    pass
-with app.app_context():
-    class Chemical(Base):
-        __table__ = Table('Chemical', Base.metadata, autoload_with=db.engine)
+
 
 
 @app.route('/')
@@ -75,7 +89,7 @@ def hello_world():
 
 @app.route('/chemicals')
 @oidc.require_login
-def get_users():
+def get_chemicals():
     return "You signed in as "+g.oidc_user.name+"<br/>"+"<br/>".join([chemical.Chemical_Name for chemical in db.session.query(Chemical).all()])
 
 if __name__ == '__main__':
