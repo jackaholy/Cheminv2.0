8b6a45f2ccdecfe57f1d2013cfb9f05a3146069b
Justin Forseth <justin@jforseth.com>
2025-02-11 14:01:05 -0700

    Start working on sql models

--- backend/src/app.py
+++ backend/src/app.py
@@ -63,9 +63,65 @@ class Chemical(Base):
     Who_Ordered = Column(String(20), nullable=True)
     Minimum_On_Hand = Column(Float, nullable=True)
     Minimum_Unit_ID = Column(Integer, ForeignKey('Unit.Unit_ID'), nullable=True)
-    
+
+    manufacturers = relationship("ChemicalManufacturer", back_populates="chemical")
     storage_class = relationship("StorageClass", back_populates="chemicals")
     minimum_unit = relationship("Unit", back_populates="chemicals")
+class ChemicalManufacturer(Base):
+    __tablename__ = 'Chemical_Manufacturer'
+
+    Chemical_Manufacturer_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Chemical_ID = Column(Integer, ForeignKey('Chemical.Chemical_ID'), nullable=False)
+    Manufacturer_ID = Column(Integer, ForeignKey('Manufacturer.Manufacturer_ID'), nullable=False)
+    Product_Number = Column(String(20), nullable=True)
+    CAS_Number = Column(String(20), nullable=True)
+    MSDS = Column(String(200), nullable=True)
+    Barcode = Column(String(200), nullable=True)
+    Comment = Column(String(50), nullable=True)
+
+    # Define relationships if needed
+    chemical = relationship("Chemical", back_populates="manufacturers")
+    manufacturer = relationship("Manufacturer", back_populates="chemicals")
+class Inventory(Base):
+    __tablename__ = 'Inventory'
+
+    Inventory_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Sticker_Number = Column(Integer, unique=True, nullable=False)
+    Chemical_Manufacturer_ID = Column(Integer, ForeignKey('Chemical_Manufacturer.Chemical_Manufacturer_ID'), nullable=False)
+    Sub_Location_ID = Column(Integer, ForeignKey('Sub_Location.Sub_Location_ID'), nullable=False)
+    Quantity = Column(Float, default=1)
+    Unit_ID = Column(Integer, ForeignKey('Unit.Unit_ID'), default=1)
+    Last_Updated = Column(Date, nullable=True)
+    Who_Updated = Column(String(30), nullable=True)
+    Is_Dead = Column(Boolean, default=False)
+    Comment = Column(String(50), nullable=True)
+    Product_Number = Column(String(20), nullable=True)
+    CAS_Number = Column(String(20), nullable=True)
+    MSDS = Column(String(200), nullable=True)
+    Barcode = Column(String(200), nullable=True)
+
+    chemical_manufacturer = relationship("ChemicalManufacturer", back_populates="inventory")
+    sub_location = relationship("SubLocation", back_populates="inventory")
+    unit = relationship("Unit", back_populates="inventory")
+
+class Location(Base):
+    __tablename__ = 'Location'
+    Location_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Room = Column(String(20), nullable=False)
+    Building = Column(String(20), nullable=False)
+class SubLocation(Base):
+    __tablename__ = 'Sub_Location'
+    Sub_Location_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Sub_Location_Name = Column(String(35), nullable=False)
+    Location_ID = Column(Integer, ForeignKey('Location.Location_ID'), nullable=False)
+    
+    location = relationship("Location", back_populates="sub_locations")
+    inventory = relationship("Inventory", back_populates="sub_location")
+class Manufacturer(Base):
+    __tablename__ = 'Manufacturer'
+    Manufacturer_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Manufacturer_Name = Column(String(30), nullable=False)
+    chemicals = relationship("ChemicalManufacturer", back_populates="manufacturer")
 
 class StorageClass(Base):
     __tablename__ = 'Storage_Class'
@@ -73,13 +129,19 @@ class StorageClass(Base):
     Storage_Class_ID = Column(Integer, primary_key=True)
     chemicals = relationship("Chemical", back_populates="storage_class")
 
+
 class Unit(Base):
     __tablename__ = 'Unit'
+    Unit_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Unit_Name = Column(String(30), unique=True, nullable=False)
+    Multiply_By = Column(Float, nullable=True)
+    to_conversion_unit = relationship("Unit", remote_side=[Unit_ID])
+
     
-    Unit_ID = Column(Integer, primary_key=True)
+    inventory = relationship("Inventory", back_populates="unit")
     chemicals = relationship("Chemical", back_populates="minimum_unit")
 
-# There's probably a better way to do this: https://stackoverflow.com/questions/39955521/sqlalchemy-existing-database-query
+
 
 
 
