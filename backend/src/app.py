7ee05bef4e0596f2788b157e7c11d4b8b79d4f41
Justin Forseth <justin@jforseth.com>
2025-02-16 14:53:25 -0700

    Start work on search api

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,5 @@
-from flask import g, Flask
-from sqlalchemy import URL, Table,  Column, Integer, String, Float, Date, ForeignKey, Boolean
+from flask import g, Flask, render_template, session, request
+from sqlalchemy import URL, Table,  Column, Integer, String, Float, Date, ForeignKey, Boolean, or_
 from flask_cors import CORS
 from sqlalchemy import URL, Table
 from sqlalchemy.exc import DatabaseError, OperationalError
@@ -10,8 +10,11 @@ import os
 from dotenv import load_dotenv
 import time
 import waitress
-from models import db, Chemical, Location
+from models import db, Chemical, Location, Inventory, Chemical_Manufacturer
 from authlib.integrations.flask_oauth2 import current_token
+import requests
+import json
+
 print("Initializing app")
 load_dotenv()
 
@@ -94,6 +97,69 @@ def get_chemicals_example():
 def get_location_example():
     return "<br/>".join([location.Building + " " + location.Room + ": " + ",".join([x.Sub_Location_Name for x in location.Sub_Locations]) for location in db.session.query(Location).all()])
 
+@app.route('/api/search', methods=['POST'])
+def search():
+    print("Request recieved")
+    query = request.json.get("query")
+    if not query: 
+        return []
+    
+    query.replace("/","")
+    query.replace("%2F","")
+    query.replace("%2f","")
+    print("Looking up in pubchem")
+    response = requests.get(f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/name/{query}/synonyms/json").json()
+    all_synonyms = [query]
+    if "InformationList" in response:
+        print("Synonyms found:")
+        for substance in response["InformationList"]["Information"]:
+            all_synonyms.extend(substance["Synonym"])
+    else: 
+        print("No synonyms found")
+    all_synonyms =  list(set(all_synonyms))
+
+    # Element symbols match all kinds of things in the database, so we filter them out
+    all_synonyms = [synonym for synonym in all_synonyms if len(synonym) > 3]
+    print("Querying the database for:")
+    #print("\t\n".join(all_synonyms))
+    matching_entries = []
+    for synonym in all_synonyms:
+        synonym_matches = db.session.query(Chemical).filter(
+            or_(
+            Chemical.Chemical_Name.like("%"+synonym+"%"), 
+            Chemical.Alphabetical_Name.like("%"+synonym+"%")
+        )).all()
+        print("Matches for " + synonym + ":")
+        print("\t\n".join([x.Alphabetical_Name for x in synonym_matches]))
+        print()
+        print()
+        matching_entries.extend(synonym_matches)
+        unique_entries = set()
+
+        for chemical in matching_entries:
+            chemical_manufacturers = db.session.query(Chemical_Manufacturer).filter(
+                Chemical_Manufacturer.Chemical_ID == chemical.Chemical_ID
+            ).all()
+
+            for chemical_manufacturer in chemical_manufacturers:
+                inventory_records = db.session.query(Inventory).filter(
+                    Inventory.Product_Number == chemical_manufacturer.Product_Number
+                ).all()
+
+                for record in inventory_records:
+                    unique_entries.add((
+                        chemical.Chemical_Name,
+                        chemical_manufacturer.Product_Number,
+                        record.Sticker_Number
+                    ))
+
+        response_entries = [
+            {"name": name, "product_number": product_number, "sticker": sticker}
+            for name, product_number, sticker in unique_entries
+        ]
+
+    return response_entries
+    
 if __name__ == '__main__':
     if os.getenv("CHEMINV_ENVIRONMENT") == "development":
         app.run(debug=True, host="0.0.0.0", port=5000)
