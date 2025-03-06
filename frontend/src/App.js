d8a8ac648bb7dc02366393b49e2ae011bc57fe35
Justin Forseth <justin@jforseth.com>
2025-03-06 11:59:19 -0700

    Begin work on implementing chemical addition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -88,7 +88,7 @@ const ChemicalModal = ({ chemical, show, handleClose }) => {
     </div>
   );
 };
-const Navbar = ({handleShowAddChemicalModal}) => {
+const Navbar = ({ handleShowAddChemicalModal }) => {
   const [user, setUser] = useState({});
 
   useEffect(() => {
@@ -104,20 +104,20 @@ const Navbar = ({handleShowAddChemicalModal}) => {
           Chemical Inventory
         </a>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link active"
-                                href="#"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    handleShowAddChemicalModal();
-                                }}
-                            >
-                                Add Chemical
-                            </a>
-                        </li>
-                    </ul>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleShowAddChemicalModal();
+                }}
+              >
+                Add Chemical
+              </a>
+            </li>
+          </ul>
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item dropdown">
               <a
@@ -163,92 +163,185 @@ const Navbar = ({handleShowAddChemicalModal}) => {
   );
 };
 
-const AddChemicalModal = ({show, handleClose}) => {
-    return (
-        <div className={`modal fade ${show ? "show d-block" : "d-none"}`} tabIndex="-1">
-            <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="addChemicalLabel">Acetic Acid</h1>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleClose}></button>
-                    </div>
-                    <div className="modal-body">
-                        {/* Identification Section */}
-                        <div className="grouped-section">
-                            <label className="form-label">Sticker Number</label>
-                            <input type="text" className="form-control" placeholder="3008" readOnly/>
-                            <label className="form-label">Chemical Name</label>
-                            <input type="text" className="form-control" placeholder="Acetic Acid" readOnly/>
-                            <label className="form-label">Chemical Formula/Common Name</label>
-                            <input type="text" className="form-control" placeholder="" readOnly/>
-                            <label className="form-label">Storage Class</label>
-                            <input type="text" className="form-control" placeholder="Corr White" readOnly/>
-                            <button type="button" className="btn btn-secondary">Select Chemical</button>
-                            <button type="button" className="btn btn-secondary">Fix Chemical Typo</button>
-                        </div>
+const AddChemicalModal = ({ show, handleClose }) => {
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState({});
 
-                        {/* Quantity Section */}
-                        <div className="grouped-section">
-                            <label className="form-label">Quantity</label>
-                            <input type="text" className="form-control" placeholder="1"/>
-                            <label className="form-label">Unit</label>
-                            <select className="form-select">
-                                <option selected>Bottle</option>
-                                <option value="Liters">Liters</option>
-                                <option value="Milliliters">Milliliters</option>
-                                <option value="Kilograms">Kilograms</option>
-                            </select>
-                        </div>
+  useEffect(() => {
+    fetch(`/api/locations`)
+      .then((response) => response.json())
+      .then((data) => setLocations(data))
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(selectedLocation);
 
-                        {/* Update Info Section */}
-                        <div className="grouped-section">
-                            <label className="form-label">Last Updated</label>
-                            <input type="text" className="form-control" placeholder="2024-09-19" readOnly/>
-                            <label className="form-label">Updated By</label>
-                            <input type="text" className="form-control" placeholder="emowat" readOnly/>
-                        </div>
-
-                        {/* Location Section */}
-                        <div className="grouped-section">
-                            <label className="form-label">Location</label>
-                            <select className="form-select">
-                                <option selected>FC 212</option>
-                                <option value="FC 101">FC 101</option>
-                                <option value="Lab 3">Lab 3</option>
-                            </select>
-                            <label className="form-label">Sub-Location</label>
-                            <select className="form-select">
-                                <option selected>Hood G</option>
-                                <option value="Shelf A">Shelf A</option>
-                                <option value="Cabinet B">Cabinet B</option>
-                            </select>
-                        </div>
-
-                        {/* Manufacturer Section */}
-                        <div className="grouped-section">
-                            <label className="form-label">Manufacturer Name</label>
-                            <select className="form-select">
-                                <option selected>TCI</option>
-                                <option value="Sigma-Aldrich">Sigma-Aldrich</option>
-                                <option value="Fisher Scientific">Fisher Scientific</option>
-                            </select>
-                            <label className="form-label">Product Number</label>
-                            <input type="text" className="form-control" placeholder="N0155"/>
-                            <label className="form-label">Material Safety Data Sheet</label>
-                            <input type="text" className="form-control" placeholder="MSDS Link" readOnly/>
-                            <button type="button" className="btn btn-secondary">Add Manufacturer</button>
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"
-                                aria-label="Close" onClick={handleClose}>Cancel
-                        </button>
-                        <button type="button" className="btn btn-primary">Save Chemical</button>
-                    </div>
-                </div>
+  return (
+    <div
+      className={`modal fade ${show ? "show d-block" : "d-none"}`}
+      tabIndex="-1"
+    >
+      <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="addChemicalLabel">
+              Acetic Acid
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={handleClose}
+            ></button>
+          </div>
+          <div className="modal-body">
+            {/* Identification Section */}
+            <div className="grouped-section">
+              <label className="form-label">Sticker Number</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="3008"
+                readOnly
+              />
+              <label className="form-label">Chemical Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Acetic Acid"
+                readOnly
+              />
+              <label className="form-label">Chemical Formula/Common Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                readOnly
+              />
+              <label className="form-label">Storage Class</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Corr White"
+                readOnly
+              />
+              <button type="button" className="btn btn-secondary">
+                Select Chemical
+              </button>
+              <button type="button" className="btn btn-secondary">
+                Fix Chemical Typo
+              </button>
             </div>
+
+            {/* Quantity Section */}
+            {/* Unused Steve Harper feature? */}
+            {/*
+            <div className="grouped-section">
+              <label className="form-label">Quantity</label>
+              <input type="text" className="form-control" placeholder="1" />
+              <label className="form-label">Unit</label>
+              <select className="form-select">
+                <option selected>Bottle</option>
+                <option value="Liters">Liters</option>
+                <option value="Milliliters">Milliliters</option>
+                <option value="Kilograms">Kilograms</option>
+              </select>
+            </div>
+            */}
+
+            {/* Update Info Section */}
+            {/* Automatically filled in with current user */}
+            {/*
+            <div className="grouped-section">
+              <label className="form-label">Last Updated</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="2024-09-19"
+                readOnly
+              />
+              <label className="form-label">Updated By</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="emowat"
+                readOnly
+              />
+            </div>
+            */}
+
+            {/* Location Section */}
+            <div className="grouped-section">
+              <label className="form-label">Location</label>
+              <select
+                className="form-select"
+                onChange={(e) =>
+                  setSelectedLocation(
+                    locations.find(
+                      (location) =>
+                        location.location_id === parseInt(e.target.value)
+                    )
+                  )
+                }
+              >
+                {locations.map((location) => (
+                  <option value={location.location_id}>
+                    {location.building} {location.room}
+                  </option>
+                ))}
+              </select>
+              <label className="form-label">Sub-Location</label>
+              <select className="form-select">
+                {selectedLocation &&
+                  selectedLocation.sub_locations &&
+                  selectedLocation.sub_locations.map((sub_location) => (
+                    <option value={sub_location.sub_location_id}>
+                      {sub_location.sub_location_name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+
+            {/* Manufacturer Section */}
+            <div className="grouped-section">
+              <label className="form-label">Manufacturer Name</label>
+              <select className="form-select">
+                <option selected>TCI</option>
+                <option value="Sigma-Aldrich">Sigma-Aldrich</option>
+                <option value="Fisher Scientific">Fisher Scientific</option>
+              </select>
+              <label className="form-label">Product Number</label>
+              <input type="text" className="form-control" placeholder="N0155" />
+              <label className="form-label">Material Safety Data Sheet</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="MSDS Link"
+                readOnly
+              />
+              <button type="button" className="btn btn-secondary">
+                Add Manufacturer
+              </button>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={handleClose}
+            >
+              Cancel
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save Chemical
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 const Sidebar = ({
@@ -413,7 +506,7 @@ const App = () => {
   const [selectedChemical, setSelectedChemical] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
-    const [showAddChemicalModal, setShowAddChemicalModal] = useState(false);
+  const [showAddChemicalModal, setShowAddChemicalModal] = useState(false);
 
   const handleShowModal = (chem) => {
     setSelectedChemical(chem);
@@ -424,13 +517,13 @@ const App = () => {
     setShowModal(false);
   };
 
-    const handleShowAddChemicalModal = () => {
-        setShowAddChemicalModal(true);
-    };
+  const handleShowAddChemicalModal = () => {
+    setShowAddChemicalModal(true);
+  };
 
-    const handleCloseAddChemicalModal = () => {
-        setShowAddChemicalModal(false);
-    };
+  const handleCloseAddChemicalModal = () => {
+    setShowAddChemicalModal(false);
+  };
 
   function handleSearch(query, synonyms = false) {
     if (query === "") {
@@ -476,7 +569,6 @@ const App = () => {
   useEffect(() => {
     getQuantity(); // Fetch chemicals on component mount
   }, []);
-
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -548,7 +640,7 @@ const App = () => {
 
   return (
     <div className="tw-bg-gray-100 pb-3">
-            <Navbar handleShowAddChemicalModal={handleShowAddChemicalModal}/>
+      <Navbar handleShowAddChemicalModal={handleShowAddChemicalModal} />
       <div className="tw-flex tw-mt-4">
         <Sidebar
           chemicals={chemicals}
@@ -567,11 +659,15 @@ const App = () => {
         />
         <ManageUsersModal />
       </div>
-            <ChemicalModal chemical={selectedChemical} show={showModal} handleClose={handleCloseModal}/>
-            <AddChemicalModal
-                show={showAddChemicalModal}
-                handleClose={handleCloseAddChemicalModal}
-            />
+      <ChemicalModal
+        chemical={selectedChemical}
+        show={showModal}
+        handleClose={handleCloseModal}
+      />
+      <AddChemicalModal
+        show={showAddChemicalModal}
+        handleClose={handleCloseAddChemicalModal}
+      />
     </div>
   );
 };
