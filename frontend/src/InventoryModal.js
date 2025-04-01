2bc01f4859ca9057dc28250f1090c5e07b9f8469
Jack Holy <jholy@carroll.edu>
2025-04-01 14:50:36 -0600

    Show all the possible rooms in the inventory dropdown

--- frontend/src/InventoryModal.js
+++ frontend/src/InventoryModal.js
@@ -1,48 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Modal from "react-bootstrap/Modal";
 export const InventoryModal = ({ show, handleClose }) => {
+    const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/locations", { credentials: "include" })
+      .then((response) => response.json())
+      .then((data) => setRooms(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <Modal show={show} onHide={handleClose} centered size="lg">
       <Modal.Header closeButton>
         <Modal.Title>Inventory</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div class="grouped-section">
-          <label class="form-label">Location</label>
-          <select class="form-select">
+        <div className="grouped-section">
+          <label className="form-label">Location</label>
+          <select className="form-select">
+
             <option selected>Pick a Location</option>
-            <option value="FC 111">FC 111</option>
-            <option value="FC 114">FC 114</option>
-            <option value="FC 115">FC 115</option>
+                {rooms.map((room) => (
+              <option key={room.Location_ID} value={room.Location_ID}>
+                {room.building} {room.room}
+              </option>
+            ))}
           </select>
 
-          <label class="form-label">Sub Location</label>
-          <select class="form-select">
+          <label className="form-label">Sub Location</label>
+          <select className="form-select">
             <option selected>Pick a Location</option>
             <option value="FC 111">Acid Cabinet</option>
             <option value="FC 114">Corr White</option>
             <option value="FC 115">Flam Red</option>
           </select>
 
-          <label class="form-label">Sticker Number</label>
-          <input type="text" class="form-control" placeholder="Type here..." />
+          <label className="form-label">Sticker Number</label>
+          <input type="text" className="form-control" placeholder="Type here..." />
 
-          <button type="button" class="btn btn-secondary">
+          <button type="button" className="btn btn-secondary">
             Search
           </button>
-          <button type="button" class="btn btn-secondary">
+          <button type="button" className="btn btn-secondary">
             Reset
           </button>
         </div>
       </Modal.Body>
       <Modal.Footer>
-        <button type="button" class="btn btn-secondary">
+        <button type="button" className="btn btn-secondary">
           Complete Sub Location
         </button>
-        <button type="button" class="btn btn-secondary">
+        <button type="button" className="btn btn-secondary">
           Complete Room
         </button>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
           Finish
         </button>
       </Modal.Footer>
