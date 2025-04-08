dfd37afc38b478a583b0b5d803006ef85de3243d
Justin Forseth <justin@jforseth.com>
2025-04-08 15:45:18 -0600

    Refactor chemical table layout and update edit button style

--- frontend/src/MainContent.js
+++ frontend/src/MainContent.js
@@ -50,7 +50,7 @@ export const MainContent = ({
       <div className="tw-flex tw-justify-between">
         {renderDownloadButton(user)}
       </div>
-      <div className="tw-grid tw-grid-cols-4 tw-border-b tw-p-2 tw-font-semibold">
+      <div className="tw-grid tw-grid-cols-[2fr_3fr_3fr_40px] tw-border-b tw-p-2 tw-font-semibold">
         <div>Quantity</div>
         <div>Chemical</div>
         <div>Chemical Formula</div>
@@ -61,7 +61,7 @@ export const MainContent = ({
           <p>Loading...</p>
         ) : (
           chemicalsData.map((chem, index) => (
-            <div key={index} className="tw-grid tw-grid-cols-4 tw-p-2">
+            <div key={index} className="tw-grid tw-grid-cols-[2fr_3fr_3fr_40px] tw-p-2">
               {/*Columns on the main page*/}
               <div>{chem.quantity}</div>
               <div>
@@ -79,10 +79,13 @@ export const MainContent = ({
               <div>{chem.formula}</div>
               <div>
                 <button
-                  className="btn btn-link"
-                  onClick={() => handleEditClick(chem)} // Trigger modal on edit click
+                  className="btn btn-secondary btn-sm"
+                  onClick={() => handleEditClick(chem)}
+                  title="Edit"
                 >
-                  Edit
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                  </svg>
                 </button>
               </div>
             </div>
