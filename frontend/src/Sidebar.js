459d973cf36ed97255032ff9276d926fc2b1e36b
Abby Brown <amb0330@icloud.com>
2025-04-09 22:40:43 -0600

    Working accordian... might change a little later.

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,158 +1,191 @@
-import { useState, useEffect, use } from "react";
+import {useState, useEffect} from "react";
 
 export const Sidebar = ({
-  query,
-  setQuery,
-  handleSearch,
-  selectedManufacturers,
-  setSelectedManufacturers,
-  selectedRoom,
-  setSelectedRoom,
-}) => {
-  const [roomFilterText, setRoomFilterText] = useState("");
-  const [locations, setRooms] = useState([]);
-  const [filteredLocations, setFilteredLocations] = useState([]);
+                            query,
+                            setQuery,
+                            handleSearch,
+                            selectedManufacturers,
+                            setSelectedManufacturers,
+                            selectedRoom,
+                            setSelectedRoom,
+                        }) => {
+    const [roomFilterText, setRoomFilterText] = useState("");
+    const [locations, setRooms] = useState([]);
+    const [filteredLocations, setFilteredLocations] = useState([]);
 
-  const [manufacturerFilterText, setManufacturerFilterText] = useState("");
-  const [manufacturers, setManufacturers] = useState([]);
-  const [filteredManufacturers, setFilteredManufacturers] = useState([]);
+    const [manufacturerFilterText, setManufacturerFilterText] = useState("");
+    const [manufacturers, setManufacturers] = useState([]);
+    const [filteredManufacturers, setFilteredManufacturers] = useState([]);
 
-  useEffect(() => {
-    if (manufacturerFilterText != "") {
-      setFilteredManufacturers(
-        manufacturers.filter((man) =>
-          man.name.toLowerCase().includes(manufacturerFilterText.toLowerCase())
-        )
-      );
-      return;
-    }
-    setFilteredManufacturers(manufacturers);
-  }, [manufacturerFilterText, manufacturers]);
+    const [isRoomAccordionOpen, setIsRoomAccordionOpen] = useState(true);
+    const [isManufacturerAccordionOpen, setIsManufacturerAccordionOpen] = useState(true);
 
-  useEffect(() => {
-    if (roomFilterText != "") {
-      setFilteredLocations(
-        locations.filter((loc) =>
-          `${loc.building} ${loc.room}`
-            .toLowerCase()
-            .includes(roomFilterText.toLowerCase())
-        )
-      );
-      return;
-    }
-    setFilteredLocations(locations);
-  }, [roomFilterText, locations]);
+    useEffect(() => {
+        if (manufacturerFilterText !== "") {
+            setFilteredManufacturers(
+                manufacturers.filter((man) =>
+                    man.name.toLowerCase().includes(manufacturerFilterText.toLowerCase())
+                )
+            );
+            return;
+        }
+        setFilteredManufacturers(manufacturers);
+    }, [manufacturerFilterText, manufacturers]);
 
-  useEffect(() => {
-    fetch("/api/locations", { credentials: "include" })
-      .then((response) => response.json())
-      .then((data) => setRooms(data))
-      .catch((error) => console.error(error));
-  }, []);
+    useEffect(() => {
+        if (roomFilterText !== "") {
+            setFilteredLocations(
+                locations.filter((loc) =>
+                    `${loc.building} ${loc.room}`
+                        .toLowerCase()
+                        .includes(roomFilterText.toLowerCase())
+                )
+            );
+            return;
+        }
+        setFilteredLocations(locations);
+    }, [roomFilterText, locations]);
 
-  useEffect(() => {
-    fetch("/api/manufacturers", { credentials: "include" })
-      .then((response) => response.json())
-      .then((data) => setManufacturers(data))
-      .catch((error) => console.error(error));
-  }, []);
+    useEffect(() => {
+        fetch("/api/locations", {credentials: "include"})
+            .then((response) => response.json())
+            .then((data) => setRooms(data))
+            .catch((error) => console.error(error));
+    }, []);
 
-  useEffect(() => {
-    handleSearch(query);
-  }, [selectedManufacturers, selectedRoom]);
+    useEffect(() => {
+        fetch("/api/manufacturers", {credentials: "include"})
+            .then((response) => response.json())
+            .then((data) => setManufacturers(data))
+            .catch((error) => console.error(error));
+    }, []);
 
-  const toggleManufacturer = (man) => {
-    setSelectedManufacturers((prev) =>
-      prev.includes(man) ? prev.filter((m) => m !== man) : [...prev, man]
-    );
-  };
+    useEffect(() => {
+        handleSearch(query);
+    }, [selectedManufacturers, selectedRoom]);
 
-  const debounceDelay = 75;
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      handleSearch(query);
-    }, debounceDelay);
+    const toggleManufacturer = (man) => {
+        setSelectedManufacturers((prev) =>
+            prev.includes(man) ? prev.filter((m) => m !== man) : [...prev, man]
+        );
+    };
 
-    return () => clearTimeout(handler);
-  }, [query]);
-  return (
-    <div className="tw-w-1/4 tw-bg-white tw-p-4 tw-rounded-md tw-shadow-md">
-      <div className="tw-flex tw-items-center tw-border tw-p-2 tw-rounded-md">
-        <form
-          className="tw-w-full tw-flex"
-          onSubmit={(e) => {
-            e.preventDefault();
+    const debounceDelay = 75;
+    useEffect(() => {
+        const handler = setTimeout(() => {
             handleSearch(query);
-          }}
-        >
-          <input
-            name="query"
-            type="text"
-            placeholder="Search..."
-            className="tw-ml-2 tw-w-full tw-outline-none"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button className="tw-flex tw-items-center tw-justify-center tw-w-8 tw-h-8 tw-bg-transparent tw-border-none hover:tw-opacity-70">
-            <span className="material-icons">search</span>
-          </button>
-        </form>
-      </div>
-      <div className="tw-mt-4">
-        <div className="tw-font-semibold">Room Location</div>
-        <input
-          className="form-control"
-          placeholder="Filter rooms"
-          value={roomFilterText}
-          onChange={(e) => setRoomFilterText(e.target.value)}
-        />
-        <div className="tw-mt-2 tw-space-y-1">
-          <label key={0} className="tw-flex tw-items-center">
-            <input
-              type="radio"
-              name="room"
-              className="tw-mr-2"
-              onChange={() => setSelectedRoom(0)}
-            />
-            Any
-          </label>
-          {filteredLocations.map((location, index) => (
-            <label key={index} className="tw-flex tw-items-center">
-              <input
-                type="radio"
-                name="room"
-                className="tw-mr-2"
-                checked={selectedRoom === location.location_id}
-                onChange={() => setSelectedRoom(location.location_id)}
-              />
-              {location.building} {location.room}
-            </label>
-          ))}
+        }, debounceDelay);
+
+        return () => clearTimeout(handler);
+    }, [query]);
+
+    return (
+        <div className="tw-w-1/4 tw-bg-white tw-p-4 tw-rounded-md tw-shadow-md">
+            {/* Search Bar */}
+            <div className="tw-flex tw-items-center tw-border tw-p-2 tw-rounded-md">
+                <form
+                    className="tw-w-full tw-flex"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleSearch(query);
+                    }}
+                >
+                    <input
+                        name="query"
+                        type="text"
+                        placeholder="Search..."
+                        className="tw-ml-2 tw-w-full tw-outline-none"
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                    <button
+                        className="tw-flex tw-items-center tw-justify-center tw-w-8 tw-h-8 tw-bg-transparent tw-border-none hover:tw-opacity-70">
+                        <span className="material-icons">search</span>
+                    </button>
+                </form>
+            </div>
+
+            {/* Room Accordion */}
+            <div className="tw-mt-4">
+                <div
+                    className="tw-font-semibold tw-cursor-pointer tw-flex tw-justify-between"
+                    onClick={() => setIsRoomAccordionOpen(!isRoomAccordionOpen)}
+                >
+                    <span>Room Location</span>
+                    <span className="material-icons">{isRoomAccordionOpen ? "expand_less" : "expand_more"}</span>
+                </div>
+
+                {isRoomAccordionOpen && (
+                    <>
+                        <input
+                            className="form-control tw-mt-1"
+                            placeholder="Filter rooms"
+                            value={roomFilterText}
+                            onChange={(e) => setRoomFilterText(e.target.value)}
+                        />
+                        <div className="tw-mt-2 tw-space-y-1">
+                            <label key={0} className="tw-flex tw-items-center">
+                                <input
+                                    type="radio"
+                                    name="room"
+                                    className="tw-mr-2"
+                                    onChange={() => setSelectedRoom(0)}
+                                />
+                                Any
+                            </label>
+                            {filteredLocations.map((location, index) => (
+                                <label key={index} className="tw-flex tw-items-center">
+                                    <input
+                                        type="radio"
+                                        name="room"
+                                        className="tw-mr-2"
+                                        checked={selectedRoom === location.location_id}
+                                        onChange={() => setSelectedRoom(location.location_id)}
+                                    />
+                                    {location.building} {location.room}
+                                </label>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div>
+
+            {/* Manufacturer Accordion */}
+            <div className="tw-mt-4">
+                <div
+                    className="tw-font-semibold tw-cursor-pointer tw-flex tw-justify-between"
+                    onClick={() => setIsManufacturerAccordionOpen(!isManufacturerAccordionOpen)}
+                >
+                    <span>Manufacturers</span>
+                    <span
+                        className="material-icons">{isManufacturerAccordionOpen ? "expand_less" : "expand_more"}</span>
+                </div>
+
+                {isManufacturerAccordionOpen && (
+                    <>
+                        <input
+                            className="form-control tw-mt-1"
+                            placeholder="Filter manufacturers"
+                            value={manufacturerFilterText}
+                            onChange={(e) => setManufacturerFilterText(e.target.value)}
+                        />
+                        <div className="tw-mt-2 tw-space-y-1">
+                            {filteredManufacturers.map((man, index) => (
+                                <label key={index} className="tw-flex tw-items-center">
+                                    <input
+                                        type="checkbox"
+                                        className="tw-mr-2"
+                                        checked={selectedManufacturers.includes(man.id)}
+                                        value={man.id}
+                                        onChange={() => toggleManufacturer(man.id)}
+                                    />
+                                    {man.name}
+                                </label>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
-      </div>
-      <div className="tw-mt-4">
-        <div className="tw-font-semibold">Manufacturers</div>
-        <input
-          className="form-control"
-          placeholder="Filter manufacturers"
-          value={manufacturerFilterText}
-          onChange={(e) => setManufacturerFilterText(e.target.value)}
-        />
-        <div className="tw-mt-2 tw-space-y-1">
-          {filteredManufacturers.map((man, index) => (
-            <label key={index} className="tw-flex tw-items-center">
-              <input
-                type="checkbox"
-                className="tw-mr-2"
-                checked={selectedManufacturers.includes(man.id)}
-                value={man.id}
-                onChange={() => toggleManufacturer(man.id)}
-              />
-              {man.name}
-            </label>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
