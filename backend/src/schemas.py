60881b6a07a3e82104cf1d4aff397ae781caa645
Justin Forseth <justin@jforseth.com>
2025-04-11 21:54:09 -0600

    Add documentation comment for input validation in AddBottleSchema

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -1,5 +1,8 @@
 from marshmallow import Schema, fields, ValidationError
 
+ # Using marshmallow to make input validation easier: 
+ # https://chatgpt.com/share/67f9e3a7-ff9c-8004-a1b5-303b778a9757
+
 class AddBottleSchema(Schema):
     sticker_number = fields.Int(required=True)
     chemical_id = fields.Int(required=True)
